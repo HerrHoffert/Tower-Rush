e766989daa62db5d4390c503f50b53876d5335b5
Josef Hoffert <josef.hoffert@gmail.com>
2019-04-23 17:42:54 +0200

    Add existing code

---
+++ Scripts/EnemyDamage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] int hitPoints = 10;
+    [SerializeField] ParticleSystem hitParticlePrefab = null;
+    [SerializeField] ParticleSystem deathParticlePrefab = null;
+    [SerializeField] AudioClip enemyHitSFX = null;
+    [SerializeField] AudioClip enemyDeathSFX = null;
+
+    AudioSource myAudioSource;
+
+    private void Start()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+
+        if (hitPoints <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        hitPoints = hitPoints - 1;
+        hitParticlePrefab.Play();
+        myAudioSource.PlayOneShot(enemyHitSFX);
+    }
+
+    private void KillEnemy()
+    {
+        // Important to instantiate before destroying this object
+        var vfx = Instantiate(deathParticlePrefab, transform.position, Quaternion.identity);
+        vfx.Play();
+
+        Destroy(vfx.gameObject, vfx.main.duration);
+
+        AudioSource.PlayClipAtPoint(enemyDeathSFX, Camera.main.transform.position);
+
+        Destroy(gameObject); // the enemy
+    }
+}
---
+++ Scripts/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Range(0.1f, 120f)]
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] EnemyMovement enemyPrefab = null;
+    [SerializeField] Transform enemyParentTransform = null;
+    [SerializeField] Text spawnedEnemies = null;
+    [SerializeField] AudioClip spawnedEnemySFX = null;
+
+    int score;
+
+    void Start()
+    {
+        StartCoroutine(RepeatedlySpawnEnemies());
+
+        spawnedEnemies.text = score.ToString();
+    }
+
+    IEnumerator RepeatedlySpawnEnemies()
+    {
+        while (true) // forever
+        {
+            AddScore();
+
+            GetComponent<AudioSource>().PlayOneShot(spawnedEnemySFX);
+
+            var newEnemy = Instantiate(enemyPrefab, transform.position, enemyPrefab.transform.rotation);
+            newEnemy.transform.parent = enemyParentTransform;
+
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+
+    private void AddScore()
+    {
+        score++;
+        spawnedEnemies.text = score.ToString();
+    }
+}
